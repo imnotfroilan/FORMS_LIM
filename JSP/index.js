3b48d150671da117ee74a7355be2e374d3389daa
Froilan Lim <froilan.lim.iics@ust.edu.ph>
2020-02-16 21:06:17 +0800

    first commit

---
+++ JSP/index.js
@@ -0,0 +1,19 @@
+function sendRequest()
+{
+    alert("Thank you for making an event");
+}
+
+function checkDate()
+{
+    var DateStart = new Date(date1)
+    var DateEnd = new Date(date2)
+
+    if (DateStart>DateEnd) 
+    {
+
+    }else
+
+    {
+        alert("Your Dates are wrong");
+    }
+}
